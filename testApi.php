bd8d88e60eaeb04e874ae617e19b740522cd5f21
Rasesh Patel <RasPat1@gmail.com>
2014-07-11 01:58:18 -0400

    successful query, remove api key from public repo

--- testApi.php
+++ testApi.php
@@ -7,14 +7,15 @@ $line = fgets($handle);
 $sName = trim($line);
 $reqURL = "https://na.api.pvp.net/api/lol/na/v1.4/summoner/by-name/";
 
-$apiKey = "PLACEHOLDER";
+include_once "authVars.php";
+$apiKey = getenv('API_KEY');
 
 // map query param names to values
-$data = array("api_key" => $APIKey);
+$data = array("api_key" => $apiKey);
 
 $urlParams = http_build_query($data);
 
-$url = $reqURL . $sName . $urlParams;
+$url = $reqURL . $sName . "?" . $urlParams;
 
 $ch = curl_init($url);
 $fp = fopen("summoner_" . $sName, "w");
